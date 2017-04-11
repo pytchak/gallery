d8249aae8cddd745d1ba253310f7ba9c06a6596d
Mike Pytchak <pytchak.md@gmail.com>
2017-04-11 16:24:12 +0300

    Переписані кнопки

--- Test_winForm/Form1.Designer.cs
+++ Test_winForm/Form1.Designer.cs
@@ -33,12 +33,18 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(67, 71);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(806, 586);
+            this.button1.MaximumSize = new System.Drawing.Size(63, 53);
+            this.button1.MinimumSize = new System.Drawing.Size(63, 53);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(63, 53);
             this.button1.TabIndex = 0;
@@ -48,9 +54,13 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(455, 2);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(178, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1221, 790);
+            this.pictureBox1.Size = new System.Drawing.Size(1203, 664);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -58,9 +68,14 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(67, 12);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(23, 74);
+            this.button2.MaximumSize = new System.Drawing.Size(149, 35);
+            this.button2.MinimumSize = new System.Drawing.Size(149, 35);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(63, 53);
+            this.button2.Size = new System.Drawing.Size(149, 35);
             this.button2.TabIndex = 5;
             this.button2.Text = "Skan";
             this.button2.UseVisualStyleBackColor = true;
@@ -68,16 +83,24 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(168, 32);
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(23, 35);
+            this.textBox3.MaximumSize = new System.Drawing.Size(149, 33);
+            this.textBox3.MinimumSize = new System.Drawing.Size(149, 33);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(159, 33);
+            this.textBox3.Size = new System.Drawing.Size(149, 33);
             this.textBox3.TabIndex = 6;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(67, 130);
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button3.Location = new System.Drawing.Point(660, 586);
+            this.button3.MaximumSize = new System.Drawing.Size(63, 53);
+            this.button3.MinimumSize = new System.Drawing.Size(63, 53);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(63, 53);
             this.button3.TabIndex = 7;
@@ -85,17 +108,41 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(40, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 22);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Задайте шлях";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(20, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Size";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1676, 838);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1431, 680);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -108,10 +155,13 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button3;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Test_winForm/Form1.cs
+++ Test_winForm/Form1.cs
@@ -15,6 +15,18 @@ namespace Test_winForm
 
     public partial class Form1 : Form
     {
+        void Cheak()
+        {
+            if (dirName == null)
+            {
+                MessageBox.Show("Задайте шлях до папки");
+            }
+        }
+       
+
+
+
+
 
         string dirName;
         int counet=0;
@@ -29,12 +41,18 @@ namespace Test_winForm
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            Cheak();
             //textBox1.Text = lines[1];
             //textBox2.Text = lines[2];
             counet++;
             pictureBox1.Image = Image.FromFile(photoSplit[counet]);
-        
 
+
+
+            FileInfo file = new FileInfo(photoSplit[counet]);
+            long size = file.Length;
+            this.label2.Text = "Розмір " + size+"кб";
         }
 
 
@@ -76,6 +94,7 @@ namespace Test_winForm
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Cheak();
 
             if (Directory.Exists(dirName))
             {
@@ -105,9 +124,13 @@ namespace Test_winForm
 
         private void button3_Click(object sender, EventArgs e)
         {
+            Cheak();
             counet--;
             pictureBox1.Image = Image.FromFile(photoSplit[counet]);
-           
+
+            FileInfo file = new FileInfo(photoSplit[counet]);
+            long size = file.Length;
+            this.label2.Text = "Розмір " + size;
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -117,6 +140,25 @@ namespace Test_winForm
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+           // label1.Text("Задайте шлях");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Cheak();
+            FileInfo file = new FileInfo(photoSplit[counet]);
+            long size = file.Length;
+            this.label2.Text = "Розмір " + size + "кб";
 
         }
     }
